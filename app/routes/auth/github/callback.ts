190bdd53b2ac14af486ff5256a8333420049e06b
Lucas Castro <lucasamonrc@gmail.com>
2025-03-04 23:07:16 -0600

    Remove new lines

--- app/routes/auth/github/callback.ts
+++ app/routes/auth/github/callback.ts
@@ -5,11 +5,8 @@ import { commitSession, getSession } from "~/sessions.server";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
-
   const user = await authenticator.authenticate("github", request);
-
   session.set("user", user);
-
   return redirect(`/`, {
     headers: {
       "Set-Cookie": await commitSession(session),
