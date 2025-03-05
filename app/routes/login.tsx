2e5e6cfd400dfb2c13bba8a37538562e98d7879e
Lucas Castro <lucasamonrc@gmail.com>
2025-03-04 22:15:25 -0600

    Set up project

---
+++ app/routes/login.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div className="text-center p-4">
+      <a
+        className="block mt-2 text-blue-500 underline hover:text-blue-600"
+        href="https://reactrouter.com/docs"
+      >
+        Sign in with GitHub
+      </a>
+    </div>
+  );
+}
